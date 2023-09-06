b10d1896b550f3b90e0d95105c6c1184c7c6e77d
Joe Nalewabau <joen@upstreet.co>
2023-09-06 15:27:43 +1000

    sortedMerge

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-
+export function merge(
+  nums1: number[],
+  m: number,
+  nums2: number[],
+  n: number
+): void {
+  // Slice the nums1 array to get the initial numbers to be compared
+  const realNums1 = nums1.slice(0, m);
+  // Contact the nums2 array to the realNums1 array and then sort it
+  const sortedResult = [...realNums1, ...nums2].sort((a, b) => a - b);
+  // Need to reset the nums1 array to the sortedResult array
+  sortedResult.forEach((num, index) => {
+    nums1[index] = num;
+  });
+}
--- test/test.index.ts
+++ test/test.index.ts
@@ -1,9 +1,34 @@
 import { expect } from "chai";
+import { merge } from "..";
 
 describe("Test Group", () => {
   it("test case 1", () => {
     // ARRANGE
+    const nums1 = [1, 2, 3, 0, 0, 0];
+    const m = 3;
+    const nums2 = [2, 5, 6];
+    const n = 3;
+
     // ACT
+    merge(nums1, m, nums2, n);
+
     // ASSERT
+    expect(nums1.length).to.equal(6);
+    expect(nums1).eql([1, 2, 2, 3, 5, 6]);
+  }); // end of test
+
+  it("test case 2", () => {
+    // ARRANGE
+    const nums1 = [0];
+    const m = 0;
+    const nums2 = [1];
+    const n = 1;
+
+    // ACT
+    merge(nums1, m, nums2, n);
+
+    // ASSERT
+    expect(nums1.length).to.equal(1);
+    expect(nums1).eql([1]);
   }); // end of test
 }); // end of describe
