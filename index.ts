de95c99953ccf6f0d65391b3d96f8bd4226f7ea6
Joe Nalewabau <joen@upstreet.co>
2023-09-06 15:35:16 +1000

    removeDuplicates

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
+// spell-checker:ignore nums
+export function removeDuplicates(nums: number[]): number {
+  let indexToUpdate = 0;
+  let uniqueValue: number | null = null;
 
+  nums.forEach((num, index) => {
+    if (uniqueValue !== nums[index]) {
+      uniqueValue = num;
+
+      nums[indexToUpdate] = num;
+      indexToUpdate++;
+    }
+  });
+
+  return indexToUpdate;
+}
+
+// const numSet = new Set<number>();
+// nums.forEach((num) => numSet.add(num));
+
+// let index = 0;
+// numSet.forEach((num) => (nums[index++] = num));
+
+// return numSet.size;
--- test/test.index.ts
+++ test/test.index.ts
@@ -1,9 +1,46 @@
+// spell-checker:ignore nums
 import { expect } from "chai";
+import { removeDuplicates } from "../index";
 
 describe("Test Group", () => {
   it("test case 1", () => {
     // ARRANGE
+    const nums = [1, 1, 2];
+
     // ACT
+    const result = removeDuplicates(nums);
+
     // ASSERT
-  }); // end of test
-}); // end of describe
+    expect(result).to.equal(2);
+    const expectedMembers = nums.splice(0, 2);
+    expect(expectedMembers).to.have.members([1, 2]);
+  });
+
+  it("test case 2", () => {
+    // ARRANGE
+    const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+
+    // ACT
+    const result = removeDuplicates(nums);
+
+    // ASSERT
+    const expectedNum = 5;
+    expect(result).to.equal(expectedNum);
+    const expectedMembers = nums.splice(0, expectedNum);
+    expect(expectedMembers).to.have.members([0, 1, 2, 3, 4]);
+  });
+
+  it("test case 3", () => {
+    // ARRANGE
+    const nums: number[] = [];
+
+    // ACT
+    const result = removeDuplicates(nums);
+
+    // ASSERT
+    const expectedNum = 0;
+    expect(result).to.equal(expectedNum);
+    const expectedMembers = nums.splice(0, expectedNum);
+    expect(expectedMembers).to.have.members([]);
+  });
+});
