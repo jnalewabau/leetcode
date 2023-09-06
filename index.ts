dfc93870f68816f382b1c188a0c8dad7e599f34b
Joe Nalewabau <joen@upstreet.co>
2023-09-06 17:48:13 +1000

    majorityElement

--- index.ts
+++ index.ts
@@ -1,1 +1,41 @@
+export function majorityElement(nums: number[]): number {
+  const numCounts = new Map<number, number>();
 
+  for (const num of nums) {
+    // Establish the count for the number
+    const newCount = (numCounts.get(num) ?? 0) + 1;
+
+    // If we are past the threshold required, return the number
+    if (newCount > nums.length / 2) {
+      return num;
+    }
+
+    // Store the count in th map
+    numCounts.set(num, newCount);
+  }
+
+  throw Error("Invalid nums passed");
+}
+
+// for (const num of nums) {
+//   const newCount = (numCounts.get(num) ?? 0) + 1;
+
+//   if (newCount > nums.length / 2) {
+//     return num;
+//   }
+
+//   numCounts.set(num, newCount);
+
+// if (numCounts.has(num)) {
+//   const currentCount = numCounts.get(num)!;
+//   const newCount = currentCount + 1;
+
+//   if (newCount > nums.length / 2) {
+//     return num;
+//   }
+
+//   numCounts.set(num, currentCount + 1);
+// } else {
+//   numCounts.set(num, 1);
+// }
+// }
--- test/test.index.ts
+++ test/test.index.ts
@@ -1,9 +1,37 @@
 import { expect } from "chai";
+import { majorityElement } from "../index";
 
 describe("Test Group", () => {
   it("test case 1", () => {
     // ARRANGE
+    const nums = [3, 2, 3];
+
     // ACT
+    const result = majorityElement(nums);
+
     // ASSERT
-  }); // end of test
-}); // end of describe
+    expect(result).to.equal(3);
+  });
+
+  it("test case 2", () => {
+    // ARRANGE
+    const nums = [2, 2, 1, 1, 1, 2, 2];
+
+    // ACT
+    const result = majorityElement(nums);
+
+    // ASSERT
+    expect(result).to.equal(2);
+  });
+
+  it("test case 3", () => {
+    // ARRANGE
+    const nums = [6];
+
+    // ACT
+    const result = majorityElement(nums);
+
+    // ASSERT
+    expect(result).to.equal(6);
+  });
+});
