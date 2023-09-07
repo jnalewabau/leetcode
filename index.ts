af1d513de714158cb979640c9af753dd08e2e541
Joe Nalewabau <joen@live.com>
2023-09-07 16:49:55 +1000

    isPalindrome

--- index.ts
+++ index.ts
@@ -1,1 +1,31 @@
+export function isPalindrome(x: number): boolean {
+  const numberString = x.toString();
 
+  for (let i = 0; i < numberString.length / 2; i++) {
+    console.log(
+      `comparing ${numberString[i]} vs ${
+        numberString[numberString.length - i - 1]
+      }`
+    );
+
+    if (numberString[i] !== numberString[numberString.length - i - 1]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// const numberString = x.toString();
+// const reversedString = numberString.split("").reverse().join("");
+
+// // console.log({ numberString });
+// // console.log({ reversedString });
+
+// return numberString === reversedString;
+
+// 88
+// 01
+
+// 121
+// 012
--- test/test.index.ts
+++ test/test.index.ts
@@ -1,9 +1,36 @@
 import { expect } from "chai";
+import { isPalindrome } from "..";
 
 describe("Test Group", () => {
-  it("test case 1", () => {
+  it("test case - 121", () => {
     // ARRANGE
     // ACT
+    const result = isPalindrome(121);
     // ASSERT
+    expect(result).to.be.true;
+  }); // end of test
+
+  it("test case - -121", () => {
+    // ARRANGE
+    // ACT
+    const result = isPalindrome(-121);
+    // ASSERT
+    expect(result).to.be.false;
+  }); // end of test
+
+  it("test case - 10", () => {
+    // ARRANGE
+    // ACT
+    const result = isPalindrome(-121);
+    // ASSERT
+    expect(result).to.be.false;
+  }); // end of test
+
+  it("test case - 8", () => {
+    // ARRANGE
+    // ACT
+    const result = isPalindrome(8);
+    // ASSERT
+    expect(result).to.be.true;
   }); // end of test
 }); // end of describe
