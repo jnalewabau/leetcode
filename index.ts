7887b10c438819dfd34f5211ef0803db9b5ee02f
Joe Nalewabau <joen@live.com>
2023-09-08 10:26:03 +1000

    containsDuplicate

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
+export function containsDuplicate(nums: number[]): boolean {
+  // Create a set from nums input
+  const numSet = new Set<number>();
+  nums.forEach((num) => numSet.add(num));
 
+  // Compare the size of the set against the input length
+  // Set will be smaller if there are duplicates
+  return numSet.size < nums.length;
+}
+
+// const numSet = new Set<number>();
+
+// for (const num of nums) {
+//   if (numSet.has(num)) {
+//     return true;
+//   }
+
+//   numSet.add(num);
+// }
+
+// return false;
+// }
--- test/test.index.ts
+++ test/test.index.ts
@@ -1,9 +1,34 @@
 import { expect } from "chai";
+import { containsDuplicate } from "..";
 
 describe("Test Group", () => {
   it("test case 1", () => {
     // ARRANGE
+    const nums = [1, 2, 3, 1];
     // ACT
+    const result = containsDuplicate(nums);
+
     // ASSERT
+    expect(result).to.be.true;
+  }); // end of test
+
+  it("test case 2", () => {
+    // ARRANGE
+    const nums = [1, 2, 3, 4];
+    // ACT
+    const result = containsDuplicate(nums);
+
+    // ASSERT
+    expect(result).to.be.false;
+  }); // end of test
+
+  it("test case 3", () => {
+    // ARRANGE
+    const nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
+    // ACT
+    const result = containsDuplicate(nums);
+
+    // ASSERT
+    expect(result).to.be.true;
   }); // end of test
 }); // end of describe
