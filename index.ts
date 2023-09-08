0acb0ba9dffb4f52b89a06f46eb8fd2b962dde20
Joe Nalewabau <joen@live.com>
2023-09-08 11:23:30 +1000

    isAnagram

--- index.ts
+++ index.ts
@@ -1,1 +1,33 @@
+export function isAnagram(s: string, t: string): boolean {
+  if (s.length !== t.length) {
+    return false;
+  }
 
+  const tempT = t.split("");
+
+  for (let i = 0; i < s.length; i++) {
+    const indexT = tempT.indexOf(s[i]);
+    // console.log(`processing: [${s[i]}] : index: ${indexT}`);
+
+    if (indexT === -1) {
+      // Can't be an anagram is it doesn't have character
+      return false;
+    }
+
+    // remove this character from tempT
+    tempT.splice(indexT, 1);
+    // console.log(`found and reduced tempT to : [${tempT}]`);
+  }
+  return true;
+}
+
+// const sSorted = s
+// .split("")
+// .sort((a, b) => a.localeCompare(b))
+// .join("");
+// const tSorted = t
+// .split("")
+// .sort((a, b) => a.localeCompare(b))
+// .join("");
+
+// return sSorted === tSorted;
--- test/test.index.ts
+++ test/test.index.ts
@@ -1,9 +1,30 @@
-import { expect } from "chai";
+import { assert, expect, should } from "chai";
+import { isAnagram } from "..";
 
 describe("Test Group", () => {
   it("test case 1", () => {
     // ARRANGE
+    const s = "anagram";
+    const t = "nagaram";
+
     // ACT
+    const result = isAnagram(s, t);
     // ASSERT
+    expect(result).to.be.true;
+    assert(result === true);
+    // result.should.be.true;
+  }); // end of test
+
+  it("test case 2", () => {
+    // ARRANGE
+    const s = "rat";
+    const t = "car";
+
+    // ACT
+    const result = isAnagram(s, t);
+    // ASSERT
+    expect(result).to.be.false;
+    assert(result === false);
+    // result.should.be.true;
   }); // end of test
 }); // end of describe
