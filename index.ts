68598a7eecfa31953d90f73419d787b7634ec0f5
Joe Nalewabau <joen@live.com>
2023-09-08 10:55:26 +1000

    validPalindrome

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
+export function isPalindrome(s: string): boolean {
+  // Convert to lowercase
+  // Empty string returns true
+  const lowercaseString = s.toLowerCase();
 
+  // Remove all non-alphanumeric chars
+  const nonAlphaString = lowercaseString.replace(/[^a-zA-Z0-9]/g, "");
+
+  // Must read backwards as well as forwards
+  const reversedString = nonAlphaString.split("").reverse().join("");
+
+  return reversedString === nonAlphaString;
+}
--- test/test.index.ts
+++ test/test.index.ts
@@ -1,9 +1,47 @@
 import { expect } from "chai";
+import { isPalindrome } from "..";
 
 describe("Test Group", () => {
+  it("test case 0", () => {
+    // ARRANGE
+    const s = "radar";
+
+    // ACT
+    const result = isPalindrome(s);
+    // ASSERT
+
+    expect(result).to.be.true;
+  }); // end of test
+
   it("test case 1", () => {
     // ARRANGE
+    const s = "A man, a plan, a canal: Panama";
+
     // ACT
+    const result = isPalindrome(s);
     // ASSERT
+
+    expect(result).to.be.true;
+  }); // end of test
+
+  it("test case 2", () => {
+    // ARRANGE
+    const s = "race a car";
+
+    // ACT
+    const result = isPalindrome(s);
+    // ASSERT
+
+    expect(result).to.be.false;
+  }); // end of test
+  it("test case 3", () => {
+    // ARRANGE
+    const s = " ";
+
+    // ACT
+    const result = isPalindrome(s);
+    // ASSERT
+
+    expect(result).to.be.true;
   }); // end of test
 }); // end of describe
