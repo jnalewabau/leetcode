f19f39459d4caa9f266d14bb7b40d2768f2f0f1f
Joe Nalewabau <joen@live.com>
2023-09-09 09:30:43 +1000

    canPlaceFlowers

--- index.ts
+++ index.ts
@@ -1,1 +1,45 @@
+export function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+  return false;
+}
+// 1  2  3  4  5  6  7  8  9
+// 0, 0, 0, 0, 0, 0, 0, 0, 0
+//    1     1     1     1
 
+// 3 = 1;
+
+// 5 = 2;
+// 7 = 3;
+// 9 = 4;
+
+// Iterate through the flower bed
+// If you find three zeros in a row increment possible flower count
+// Return true if the n asked for is less <= possible flower count
+
+// NOTE: you could exist early if you meet n requirement
+// let zeroCount = 0;
+// let possibleFlowers = 0;
+
+// for (const bed of flowerbed) {
+//   if (bed === 1) {
+//     // Work out the potential beds based on the zeroCount
+//     if (zeroCount === 3) {
+//       possibleFlowers += 1;
+//     } else if (zeroCount > 3 && zeroCount % 2 === 1) {
+//       possibleFlowers = Math.floor(zeroCount / 3) + 1;
+//     }
+
+//     zeroCount = 0;
+//   } else {
+//     zeroCount++;
+//   }
+// }
+
+// if (zeroCount >= 3) {
+//   if (zeroCount === 3) {
+//     possibleFlowers += 1;
+//   } else if (zeroCount > 3 && zeroCount % 2 === 1) {
+//     possibleFlowers = Math.floor(zeroCount / 3) + 1;
+//   }
+// }
+
+// return n <= possibleFlowers;
--- test/test.index.ts
+++ test/test.index.ts
@@ -1,9 +1,44 @@
 import { expect } from "chai";
+import { canPlaceFlowers } from "..";
 
 describe("Test Group", () => {
   it("test case 1", () => {
     // ARRANGE
+    const flowerbed = [1, 0, 0, 0, 1, 0, 0, 0];
+    const n = 1;
     // ACT
+    const result = canPlaceFlowers(flowerbed, n);
     // ASSERT
+    expect(result).to.be.true;
+  }); // end of test
+
+  it("test case 2", () => {
+    // ARRANGE
+    const flowerbed = [1, 0, 0, 0, 1];
+    const n = 2;
+    // ACT
+    const result = canPlaceFlowers(flowerbed, n);
+    // ASSERT
+    expect(result).to.be.false;
+  }); // end of test
+
+  it("test case 3", () => {
+    // ARRANGE
+    const flowerbed = [1, 0, 0, 0, 0, 0, 1];
+    const n = 2;
+    // ACT
+    const result = canPlaceFlowers(flowerbed, n);
+    // ASSERT
+    expect(result).to.be.true;
+  }); // end of test
+
+  it.only("test case 3", () => {
+    // ARRANGE
+    const flowerbed = [1, 0, 0, 0, 1, 0, 0];
+    const n = 2;
+    // ACT
+    const result = canPlaceFlowers(flowerbed, n);
+    // ASSERT
+    expect(result).to.be.true;
   }); // end of test
 }); // end of describe
