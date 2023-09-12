dc3491902d72a4952416e8ed685fafc0c181af6e
Joe Nalewabau <joen@live.com>
2023-09-12 14:28:42 +1000

    indexOfFirstOccurence

--- test/test.index.ts
+++ test/test.index.ts
@@ -1,9 +1,26 @@
 import { expect } from "chai";
+import { strStr } from "..";
 
 describe("Test Group", () => {
   it("test case 1", () => {
     // ARRANGE
+    const haystack = "sadbutsad";
+    const needle = "sad";
+
     // ACT
+    const result = strStr(haystack, needle);
     // ASSERT
+    expect(result).to.equal(0);
+  }); // end of test
+
+  it("test case 1", () => {
+    // ARRANGE
+    const haystack = "leetcode";
+    const needle = "leeto";
+
+    // ACT
+    const result = strStr(haystack, needle);
+    // ASSERT
+    expect(result).to.equal(-1);
   }); // end of test
 }); // end of describe
