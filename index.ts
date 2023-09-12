b6f1301c67f03c2061450948e72a3454e5225a9b
Joe Nalewabau <joen@live.com>
2023-09-12 14:51:28 +1000

    isSubsequence

--- index.ts
+++ index.ts
@@ -1,1 +1,39 @@
+export function isSubsequence(s: string, t: string): boolean {
+  let tIndex = 0;
+  for (let i = 0; i < s.length; i++) {
+    const charToFind = s[i];
+    // console.log({ charToFind });
+    // console.log({ tIndex });
 
+    // Find the index of s[i] in t
+    tIndex = t.indexOf(charToFind, tIndex);
+
+    if (tIndex === -1) {
+      return false;
+    }
+
+    // console.log({ tToSearch });
+  }
+
+  return true;
+}
+
+// let tToSearch = t;
+// for (let i = 0; i < s.length; i++) {
+//   const charToFind = s[i];
+//   // console.log({ charToFind });
+//   // console.log({ tToSearch });
+
+//   // Find the index of s[i] in t
+//   const indexInT = tToSearch.indexOf(charToFind);
+
+//   if (indexInT === -1) {
+//     return false;
+//   }
+
+//   // We have found the character, so take
+//   tToSearch = tToSearch.slice(indexInT + 1);
+//   // console.log({ tToSearch });
+// }
+
+// return true;
--- test/test.index.ts
+++ test/test.index.ts
@@ -1,9 +1,36 @@
 import { expect } from "chai";
+import { isSubsequence } from "..";
 
 describe("Test Group", () => {
   it("test case 1", () => {
     // ARRANGE
+    const s = "abc";
+    const t = "ahbgdc";
+
     // ACT
+    const result = isSubsequence(s, t);
     // ASSERT
+    expect(result).to.be.true;
+  }); // end of test
+  it("test case 2", () => {
+    // ARRANGE
+    const s = "axc";
+    const t = "ahbgdc";
+
+    // ACT
+    const result = isSubsequence(s, t);
+    // ASSERT
+    expect(result).to.be.false;
+  }); // end of test
+
+  it("test case 3", () => {
+    // ARRANGE
+    const s = "axc";
+    const t = "axc";
+
+    // ACT
+    const result = isSubsequence(s, t);
+    // ASSERT
+    expect(result).to.be.true;
   }); // end of test
 }); // end of describe
