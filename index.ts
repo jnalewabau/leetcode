95a95811e0163d631ec282af654aa5a83573ba61
Joe Nalewabau <joen@live.com>
2023-09-12 14:24:46 +1000

    lengthOfLastWord

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
+export function lengthOfLastWord(s: string): number {
+  const trimmedEnd = s.trimEnd();
 
+  const lastSpaceIndex = trimmedEnd.lastIndexOf(" ");
+
+  if (lastSpaceIndex === -1) {
+    return trimmedEnd.length;
+  }
+
+  return trimmedEnd.length - lastSpaceIndex - 1;
+
+  // // console.log({ trimmedEnd });
+  // const splitString = trimmedEnd.split(" ");
+  // // console.log({ splitString });
+
+  // return splitString[splitString.length - 1].length;
+}
--- test/test.index.ts
+++ test/test.index.ts
@@ -1,9 +1,30 @@
 import { expect } from "chai";
+import { lengthOfLastWord } from "..";
 
 describe("Test Group", () => {
   it("test case 1", () => {
     // ARRANGE
+    const s = "Hello World";
     // ACT
+    const result = lengthOfLastWord(s);
     // ASSERT
+    expect(result).to.equal(5);
+  }); // end of test
+
+  it("test case 2", () => {
+    // ARRANGE
+    const s = "   fly me   to   the moon  ";
+    // ACT
+    const result = lengthOfLastWord(s);
+    // ASSERT
+    expect(result).to.equal(4);
+  }); // end of test
+  it("test case 3", () => {
+    // ARRANGE
+    const s = "luffy is still joyboy";
+    // ACT
+    const result = lengthOfLastWord(s);
+    // ASSERT
+    expect(result).to.equal(6);
   }); // end of test
 }); // end of describe
