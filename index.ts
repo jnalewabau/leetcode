cb78fc82981e712935a6e93c05d3f4d93e05ff6b
Joe Nalewabau <joen@live.com>
2023-09-14 15:07:09 +1000

    wordPattern

--- index.ts
+++ index.ts
@@ -1,1 +1,40 @@
+export function wordPattern(pattern: string, s: string): boolean {
+  const patternToStringMap = new Map<string, string>();
+  const mappedValues = new Set<string>();
 
+  const splitS = s.split(" ");
+
+  // Length of pattern should match the number of words
+  if (pattern.length !== splitS.length) {
+    return false;
+  }
+
+  // Build up the mapping table
+  for (let i = 0; i < pattern.length; i++) {
+    const p = pattern[i];
+    const word = splitS[i];
+    // console.log({ p });
+
+    if (patternToStringMap.has(p) === false) {
+      // Check to see whether this word has already been mapped
+      if (mappedValues.has(word)) {
+        return false;
+      }
+      // Store the associated word with the pattern
+      patternToStringMap.set(p, word);
+      mappedValues.add(word);
+    }
+  }
+
+  //Create a string from the pattern and compare
+  let expectedString = "";
+  for (let i = 0; i < pattern.length; i++) {
+    const p = pattern[i];
+    expectedString += patternToStringMap.get(p)! + " ";
+  }
+
+  expectedString = expectedString.trimEnd();
+  // console.log({ expectedString });
+  // console.log({ s });
+  return expectedString === s;
+}
--- test/test.index.ts
+++ test/test.index.ts
@@ -1,9 +1,52 @@
 import { expect } from "chai";
+import { wordPattern } from "..";
 
 describe("Test Group", () => {
   it("test case 1", () => {
     // ARRANGE
+    const pattern = "abba",
+      s = "dog cat cat dog";
+
     // ACT
+    const result = wordPattern(pattern, s);
+
     // ASSERT
+    expect(result).to.be.true;
+  }); // end of test
+
+  it("test case 2", () => {
+    // ARRANGE
+    const pattern = "abba",
+      s = "dog cat cat fish";
+
+    // ACT
+    const result = wordPattern(pattern, s);
+
+    // ASSERT
+    expect(result).to.be.false;
+  }); // end of test
+
+  it("test case 2", () => {
+    // ARRANGE
+    const pattern = "aaaa",
+      s = "dog cat cat dog";
+
+    // ACT
+    const result = wordPattern(pattern, s);
+
+    // ASSERT
+    expect(result).to.be.false;
+  }); // end of test
+
+  it("test case 3", () => {
+    // ARRANGE
+    const pattern = "abba",
+      s = "dog dog dog dog";
+
+    // ACT
+    const result = wordPattern(pattern, s);
+
+    // ASSERT
+    expect(result).to.be.false;
   }); // end of test
 }); // end of describe
