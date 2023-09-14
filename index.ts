9f7a69699dde50cfe61d086a10754eabe98ab097
Joe Nalewabau <joen@live.com>
2023-09-14 16:01:09 +1000

    happyNumber

--- index.ts
+++ index.ts
@@ -1,1 +1,46 @@
+export function isHappy(n: number): boolean {
+  // Keep a track of the squared totals
+  const calculatedNumbers = new Set<number>();
 
+  while (true) {
+    const calculation = squaredTotal(n);
+
+    // console.log({ calculation });
+
+    if (calculation === 1) {
+      return true;
+    }
+
+    if (calculatedNumbers.has(calculation)) {
+      // console.log(`Seen ${calculation}`);
+      return false;
+    }
+
+    calculatedNumbers.add(calculation);
+    // console.log({ calculatedNumbers });
+
+    n = calculation;
+  }
+}
+
+function squaredTotal(n: number): number {
+  // Convert the number to a string
+  const numberString = n.toString();
+  const digits = numberString.split("");
+
+  const squaredNums = digits.map(
+    (digit) => Number.parseInt(digit) * Number.parseInt(digit)
+  );
+
+  // console.log({ digits });
+  // console.log({ squaredNums });
+
+  // Add the squared numbers together
+  const squaredTotal = squaredNums.reduce(
+    (prev, currentValue) => prev + currentValue
+  );
+
+  // console.log({ squaredTotal });
+
+  return squaredTotal;
+}
--- test/test.index.ts
+++ test/test.index.ts
@@ -1,9 +1,22 @@
 import { expect } from "chai";
+import { isHappy } from "..";
 
 describe("Test Group", () => {
   it("test case 1", () => {
     // ARRANGE
+    const n = 19;
     // ACT
+    const result = isHappy(n);
     // ASSERT
+    expect(result).to.be.true;
+  }); // end of test
+
+  it("test case 2", () => {
+    // ARRANGE
+    const n = 2;
+    // ACT
+    const result = isHappy(n);
+    // ASSERT
+    expect(result).to.be.false;
   }); // end of test
 }); // end of describe
