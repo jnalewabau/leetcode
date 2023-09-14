86c81d5bef194512c8ee748768224d2704ece5dd
Joe Nalewabau <joen@live.com>
2023-09-14 13:45:06 +1000

    ransomNote

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
+export function canConstruct(ransomNote: string, magazine: string): boolean {
+  let magazineToBeUsed = magazine.split("");
 
+  for (const rChar of ransomNote) {
+    // console.log({ rChar });
+
+    const index = magazineToBeUsed.indexOf(rChar);
+
+    if (index === -1) {
+      // console.log(` - Not found`);
+      return false;
+    }
+
+    // Remove rChar from magazineToBeUsed
+    magazineToBeUsed.splice(index, 1);
+    // console.log({ magazineToBeUsed });
+  }
+  return true;
+}
--- test/test.index.ts
+++ test/test.index.ts
@@ -1,9 +1,34 @@
 import { expect } from "chai";
+import { canConstruct } from "..";
 
 describe("Test Group", () => {
   it("test case 1", () => {
     // ARRANGE
+    const ransomNote = "a",
+      magazine = "b";
     // ACT
+    const result = canConstruct(ransomNote, magazine);
     // ASSERT
+    expect(result).to.be.false;
+  }); // end of test
+
+  it("test case 2", () => {
+    // ARRANGE
+    const ransomNote = "aa",
+      magazine = "ab";
+    // ACT
+    const result = canConstruct(ransomNote, magazine);
+    // ASSERT
+    expect(result).to.be.false;
+  }); // end of test
+
+  it("test case 3", () => {
+    // ARRANGE
+    const ransomNote = "aa",
+      magazine = "aab";
+    // ACT
+    const result = canConstruct(ransomNote, magazine);
+    // ASSERT
+    expect(result).to.be.true;
   }); // end of test
 }); // end of describe
