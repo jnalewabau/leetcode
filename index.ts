d1a88b2ceedcaa495c09dfc839025cd6149601fe
Joe Nalewabau <joen@live.com>
2023-09-14 14:34:16 +1000

    isIsomorphic

--- index.ts
+++ index.ts
@@ -1,1 +1,52 @@
+export function isIsomorphic(s: string, t: string): boolean {
+  // console.log({ s });
+  // console.log({ t });
 
+  if (s.length !== t.length) {
+    return false;
+  }
+  const lengthOfStrings = s.length;
+
+  const modifiedS = s.split("");
+  const charMapping = new Map<string, string>();
+  const mapped = new Set<string>();
+
+  for (let i = 0; i < lengthOfStrings; i++) {
+    const sChar = s[i];
+    const tChar = t[i];
+
+    console.log(`${sChar} => ${tChar}`);
+
+    // Update the character map for this character
+    if (charMapping.has(sChar)) {
+      // Does it match the mapping we currently have?
+      if (charMapping.get(sChar)! !== tChar) {
+        // console.log(
+        //   `Mismatched char mapping ${sChar} is matched to ${charMapping.get(
+        //     sChar
+        //   )} NOT ${tChar}`
+        // );
+        return false;
+      }
+    } else {
+      // Also make sure that tChar is not in the map (as a key)
+      if (mapped.has(tChar)) {
+        // console.log(
+        //   `tChar ${tChar} is already on map ${charMapping.get(tChar)}`
+        // );
+        return false;
+      }
+
+      charMapping.set(sChar, tChar);
+      mapped.add(tChar);
+    }
+
+    modifiedS[i] = tChar;
+    // console.log({ charMapping });
+    // console.log({ modifiedS });
+  }
+
+  // console.log(`Final modification - ${modifiedS}`);
+
+  return modifiedS.join("") === t;
+}
--- test/test.index.ts
+++ test/test.index.ts
@@ -1,9 +1,42 @@
 import { expect } from "chai";
+import { isIsomorphic } from "..";
 
 describe("Test Group", () => {
   it("test case 1", () => {
     // ARRANGE
+    const s = "egg",
+      t = "add";
     // ACT
+    const result = isIsomorphic(s, t);
     // ASSERT
+    expect(result).to.be.true;
+  }); // end of test
+  it("test case 2", () => {
+    // ARRANGE
+    const s = "foo",
+      t = "bar";
+    // ACT
+    const result = isIsomorphic(s, t);
+    // ASSERT
+    expect(result).to.be.false;
+  }); // end of test
+  it("test case 3", () => {
+    // ARRANGE
+    const s = "title",
+      t = "paper";
+    // ACT
+    const result = isIsomorphic(s, t);
+    // ASSERT
+    expect(result).to.be.true;
+  }); // end of test
+
+  it("test case 4", () => {
+    // ARRANGE
+    const s = "badc",
+      t = "baba";
+    // ACT
+    const result = isIsomorphic(s, t);
+    // ASSERT
+    expect(result).to.be.false;
   }); // end of test
 }); // end of describe
