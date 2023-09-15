01f34f05ccbed2628cb15b9544cf5dd14e167bb7
Joe Nalewabau <joen@live.com>
2023-09-15 15:12:44 +1000

    summaryRanges

--- index.ts
+++ index.ts
@@ -1,1 +1,39 @@
+export function summaryRanges(nums: number[]): string[] {
+  if (nums.length == 0) {
+    return [];
+  }
 
+  if (nums.length == 1) {
+    return [nums[0].toString()];
+  }
+
+  nums.push(0);
+  let rangeStart = nums[0];
+  let rangeEnd = nums[0];
+
+  const rangeDescriptions: string[] = [];
+
+  for (let i = 1; i < nums.length; i++) {
+    const currentNum = nums[i];
+    // console.log({ currentNum });
+
+    // Check end number - if it's in sequence then extend the range
+    if (nums[i] === rangeEnd + 1) {
+      rangeEnd = currentNum;
+      continue;
+    }
+
+    // Range has finished so we push a string based on range start
+    // and rangeEnd
+    if (rangeEnd === rangeStart) {
+      rangeDescriptions.push(`${rangeStart}`);
+    } else {
+      rangeDescriptions.push(`${rangeStart}->${rangeEnd}`);
+    }
+
+    rangeStart = rangeEnd = nums[i];
+  }
+
+  // console.log({ ranges });
+  return rangeDescriptions;
+}
--- test/test.index.ts
+++ test/test.index.ts
@@ -1,9 +1,40 @@
 import { expect } from "chai";
+import { summaryRanges } from "..";
 
 describe("Test Group", () => {
+  it("test case -empty", () => {
+    // ARRANGE
+    const nums: number[] = [];
+    // ACT
+    const result = summaryRanges(nums);
+    // ASSERT
+    expect(result).eql([]);
+  }); // end of test
+
+  it("test case - 1 entry", () => {
+    // ARRANGE
+    const nums = [123];
+    // ACT
+    const result = summaryRanges(nums);
+    // ASSERT
+    expect(result).eql(["123"]);
+  }); // end of test
+
   it("test case 1", () => {
     // ARRANGE
+    const nums = [0, 1, 2, 4, 5, 7];
     // ACT
+    const result = summaryRanges(nums);
     // ASSERT
+    expect(result).eql(["0->2", "4->5", "7"]);
+  }); // end of test
+
+  it("test case 1", () => {
+    // ARRANGE
+    const nums = [0, 2, 3, 4, 6, 8, 9];
+    // ACT
+    const result = summaryRanges(nums);
+    // ASSERT
+    expect(result).eql(["0", "2->4", "6", "8->9"]);
   }); // end of test
 }); // end of describe
