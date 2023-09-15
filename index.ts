8e01f02def62bc62fc39d35c080438ee5a5d132f
Joe Nalewabau <joen@live.com>
2023-09-15 16:02:55 +1000

    validParens

--- index.ts
+++ index.ts
@@ -1,1 +1,42 @@
+export function isValid(s: string): boolean {
+  if (s.length % 2) {
+    return false;
+  }
 
+  const parenthesesMap = new Map<string, string>();
+
+  parenthesesMap.set("{", "}");
+  parenthesesMap.set("(", ")");
+  parenthesesMap.set("[", "]");
+
+  const stack = [];
+  for (let char of s) {
+    // console.log({ char });
+    // console.log({ stack });
+
+    // Is this a left paren
+    if (parenthesesMap.has(char) === true) {
+      // Push the matching close parameter to the stack
+      const expectedCloseParen = parenthesesMap.get(char)!;
+      // console.log(
+      //   `  ${char} - Open paren found - pushing matching close paren - ${expectedCloseParen}`
+      // );
+      stack.push(expectedCloseParen);
+      continue;
+    }
+
+    // Must be a right paren
+    const expectedCloseParen = stack.pop();
+    // console.log(
+    //   `  ${char} - close paren - expect it to match ${expectedCloseParen}`
+    // );
+
+    if (char !== expectedCloseParen) {
+      // Mismatch - return false;
+      return false;
+    }
+  }
+
+  // console.log({ stackLength: stack.length });
+  return stack.length === 0;
+}
--- test/test.index.ts
+++ test/test.index.ts
@@ -1,9 +1,67 @@
 import { expect } from "chai";
+import { isValid } from "..";
 
 describe("Test Group", () => {
+  it("test case - zero length string", () => {
+    // ARRANGE
+    const s = "";
+    // ACT
+    const result = isValid(s);
+    // ASSERT
+    expect(result).to.be.true;
+  }); // end of test
+
+  it("test case - odd length string", () => {
+    // ARRANGE
+    const s = "(";
+    // ACT
+    const result = isValid(s);
+    // ASSERT
+    expect(result).to.be.false;
+  }); // end of test
+
+  it("test case - odd length string", () => {
+    // ARRANGE
+    const s = "(){";
+    // ACT
+    const result = isValid(s);
+    // ASSERT
+    expect(result).to.be.false;
+  }); // end of test
+
   it("test case 1", () => {
     // ARRANGE
+    const s = "()";
     // ACT
+    const result = isValid(s);
     // ASSERT
+    expect(result).to.be.true;
+  }); // end of test
+
+  it("test case 2", () => {
+    // ARRANGE
+    const s = "()[]{}";
+    // ACT
+    const result = isValid(s);
+    // ASSERT
+    expect(result).to.be.true;
+  }); // end of test
+
+  it("test case 2", () => {
+    // ARRANGE
+    const s = "(]";
+    // ACT
+    const result = isValid(s);
+    // ASSERT
+    expect(result).to.be.false;
+  }); // end of test
+
+  it.only("test case 3", () => {
+    // ARRANGE
+    const s = "{[]}";
+    // ACT
+    const result = isValid(s);
+    // ASSERT
+    expect(result).to.be.true;
   }); // end of test
 }); // end of describe
